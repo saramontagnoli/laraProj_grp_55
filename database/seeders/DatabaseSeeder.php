1a69c184f47b5d592fb0664550bb1f623b6e1739
saramontagnoli <s1098041@studenti.univpm.it>
2023-07-31 14:48:13 +0200

    Scrittura seed della tabella occupazione

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('occupazione')->insert([
+            ['codice_occupazione' => 1, 'nome_occupazione' => 'Libero professionista'],
+            ['codice_occupazione' => 2, 'nome_occupazione' => 'Dipendente'],
+            ['codice_occupazione' => 3, 'nome_occupazione' => 'Operaio'],
+            ['codice_occupazione' => 4, 'nome_occupazione' => 'Studente'],
+            ['codice_occupazione' => 5, 'nome_occupazione' => 'Militare'],
+            ['codice_occupazione' => 6, 'nome_occupazione' => 'In pensione'],
+            ['codice_occupazione' => 7, 'nome_occupazione' => 'Disoccupato'],
+            ['codice_occupazione' => 8, 'nome_occupazione' => 'Altro']
+        ]);
+
     }
 }
