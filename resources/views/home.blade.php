c52f63d4b77f4039c5e7d7e89d448f3c2d1b6628
giadarem <95341357+giadarem@users.noreply.github.com>
2023-08-02 15:13:24 +0200

    Add files via upload
    vista home con carosello

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.struttura')
+@section('content')
+
+<div class="slideshow-container">
+
+    <div class="mySlides fade">
+        <img src="{{ asset('assets/img/audi_r8.jpg') }}" style="width:100%">
+    </div>
+
+    <div class="mySlides fade">
+        <img src="{{ asset('assets/img/lancia_ypsilon.png') }}" style="width:100%">
+    </div>
+
+    <div class="mySlides fade">
+        <img src="{{ asset('assets/img/bmw_m4.jpg') }}" style="width:100%">
+    </div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+</div>
+
+<script src="{{ asset('assets/js/carosello.js') }}"></script>
+@endsection
