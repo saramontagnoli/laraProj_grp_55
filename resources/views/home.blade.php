1d84f3e7794743b20c5b333eb43e140e38a72afd
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-03 16:22:23 +0200

    Aggiunta commenti generici slideshow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,34 @@
 
 @section('content')
 
-<div class="slideshow-container">
+    <!-- Slideshow automatico di immagini ogni 4 secondi -->
+    <div class="slideshow-container">
 
-    <div class="mySlides fade">
-        <img src="{{ asset('assets/img/audi_r8.jpg') }}" style="width:100%; height: 600px">
+        <!-- Prima immagine dello slideshow -->
+        <div class="mySlides fade">
+            <img src="{{ asset('assets/img/audi_r8.jpg') }}" style="width:100%; height: 600px">
+        </div>
+
+        <!-- Seconda immagine dello slideshow -->
+        <div class="mySlides fade">
+            <img src="{{ asset('assets/img/lancia_ypsilon.png') }}" style="width:100%; height: 600px">
+        </div>
+
+        <!-- Terza immagine dello slideshow -->
+        <div class="mySlides fade">
+            <img src="{{ asset('assets/img/bmw_m4.jpg') }}" style="width:100%; height: 600px">
+        </div>
+
+    </div>
+    <br>
+
+    <!-- Puntini di scorrimento delle immagini -->
+    <div style="text-align:center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
     </div>
 
-    <div class="mySlides fade">
-        <img src="{{ asset('assets/img/lancia_ypsilon.png') }}" style="width:100%; height: 600px">
-    </div>
-
-    <div class="mySlides fade">
-        <img src="{{ asset('assets/img/bmw_m4.jpg') }}" style="width:100%; height: 600px">
-    </div>
-
-</div>
-<br>
-
-<div style="text-align:center">
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span>
-</div>
-
-<script src="{{ asset('assets/js/carosello.js') }}"></script>
+    <!-- Inclusione dello script JS che permette di far scorrere in automatico le immagini-->
+    <script src="{{ asset('assets/js/carosello.js') }}"></script>
 @endsection
