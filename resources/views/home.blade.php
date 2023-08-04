76798f35a01f8993b4254df1647d5d28b01848f9
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-04 15:14:35 +0200

    Sistemazione commenti CSS e aggiunta sito responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,23 +39,28 @@
     <!-- Sezione che identifica la riga per le colonne COME NOLEGGIARE, CHI SIAMO e FAQ -->
     <div class="riga">
 
+        <!-- Prima sezione cliccabile contenente immagine e titolo della colonna -->
         <a href="">
             <div class="colonna_click separatore" style="background-color: transparent">
+                <!-- Immagine e titolo di COME NOLEGGIARE -->
                 <img src="{{asset('assets/img/icona_contatti.jpeg')}}" alt="come noleggiare un'auto" class="imm">
                 <h3 class="titolo">COME NOLEGGIARE</h3>
             </div>
         </a>
 
+        <!-- Seconda sezione cliccabile contenente immagine e titolo della colonna -->
         <a href="">
             <div class="colonna_click separatore" style="background-color: transparent">
-
+                <!-- Immagine e titolo di CHI SIAMO -->
                 <img src="{{asset('assets/img/icona_chisiamo.png')}}" alt="informazioni sull'azienda" class="imm">
                 <h3 class="titolo">CHI SIAMO</h3>
             </div>
         </a>
 
+        <!-- Terza sezione cliccabile contenente immagine e titolo della colonna -->
         <a href="">
             <div class="colonna_click" style="background-color: transparent">
+                <!-- Immagine e titolo di FAQ -->
                 <img src="{{asset('assets/img/icona_faq.jpg')}}" alt="domande più frequenti" class="imm">
                 <h3 class="titolo">F.A.Q.</h3>
             </div>
