61642ed7c2e8eee1ce502f64c8302e999833088c
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-04 14:08:37 +0200

    Sistemazione formattazione espaziatura navbar e button

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,19 +1,24 @@
 <!-- Footer del sito -->
 <footer>
+    <!-- Sezione del contenuto del footer -->
     <div>
+        <!-- Sezione del footer che identifica la prima riga contenente i loghi dei social -->
         <div class="rigafoot">
+            <!-- Loghi social cliccabili -->
             <a href="#" class="link_footer"><i class="fa fa-facebook"></i></a>
             <a href="#" class="link_footer"><i class="fa fa-instagram"></i></a>
             <a href="#" class="link_footer"><i class="fa fa-youtube"></i></a>
             <a href="#" class="link_footer"><i class="fa fa-twitter"></i></a>
         </div>
 
+        <!-- Sezione del footer che ientifica la seconda riga contenente dei link ai contenuti del sito -->
         <div class="rigafoot">
+            <!-- Lista di link ai contenuti della pagina -->
             <ul>
-                <li><a href="#" class="link_footer">Contatti</a></li>
                 <li><a href="#" class="link_footer">Auto</a></li>
+                <li><a href="#" class="link_footer">Come noleggiare</a></li>
+                <li><a href="#" class="link_footer">Contatti</a></li>
                 <li><a href="#" class="link_footer">Privacy Policy</a></li>
-                <li><a href="#" class="link_footer">Terms & Conditions</a></li>
             </ul>
         </div>
 
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,20 @@
 <!-- Barra di navigazione del sito -->
 <nav class="navbar">
+
     <!-- Button con nome del sito, funge da home button -->
-    <a href="" class="element_navbar posizione_sx">Formula Rent</a>
+    <div class="posizione_sx">
+        <a href="" class="element_navbar">Formula Rent</a>
+    </div>
+
 
     <!-- Opzioni di navigazione del sito sempre disponibili nella barra di navigazione in alto -->
-    <a href="" class="element_navbar posizione_dx">Login</a>
-    <a href="" class="element_navbar posizione_dx">Registrazione</a>
-    <a href="" class="element_navbar posizione_dx">Chi siamo</a>
-    <a href="" class="element_navbar posizione_dx">Come noleggiare</a>
-    <a href="" class="element_navbar posizione_dx">Auto</a>
+    <div class="posizione_dx">
+        <a href="" class="element_navbar ">Auto</a>
+        <a href="" class="element_navbar ">Come noleggiare</a>
+        <a href="" class="element_navbar ">Chi siamo</a>
+        <a href="" class="element_navbar ">Registrazione</a>
+        <a href="" class="element_navbar ">Login</a>
+    </div>
+
+
 </nav>
