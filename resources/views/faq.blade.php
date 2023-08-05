44170707cc2b89b18d5fb6a1ed14af3dacd02163
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-05 15:32:45 +0200

    Sistemazione pagina F.A.Q.

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <!-- Titolo pagina F.A.Q. -->
-    <h2 class="posizione_cx"><i><i class="fa fa-question-circle"></i>&nbsp; PAGINA F.A.Q. &nbsp;</i><i class="fa fa-question-circle"></i></h2>
+    <h2 class="posizione_cx titolo_faq"><i class="fa fa-question-circle"></i>&nbsp; PAGINA F.A.Q. &nbsp;<i class="fa fa-question-circle"></i></h2>
 
     <!-- Sezione della pagina della F.A.Q. destinata alla spiegazione di cosa sono le F.A.Q. e quali argomenti riguardano -->
     <div class="posizione_cx margini_testo">
