e9bde8c1c08fb092ff81040e6236e953f1566c0e
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 14:39:41 +0200

    Sistemazione variabili all'interno del JS delle F.A.Q.

--- public/assets/js/faq.js
+++ public/assets/js/faq.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("domanda");
+var domande = document.getElementsByClassName("domanda");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+for (i = 0; i < domande.length; i++) {
+    domande[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
+        var risposta = this.nextElementSibling;
+        if (risposta.style.maxHeight) {
+            risposta.style.maxHeight = null;
         } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
+            risposta.style.maxHeight = risposta.scrollHeight + "px";
         }
     });
 }
