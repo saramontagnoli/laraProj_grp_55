bba698fe6238959c6cd0693676e1e585ef4cd461
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 14:16:58 +0200

    Caricamento catalogoauto.blade.php

--- resources/views/catalogoauto.blade.php
+++ resources/views/catalogoauto.blade.php
@@ -3,47 +3,36 @@
 @section('content')
 
     <!-- Sezione dedicata al catalogo delle auto noleggiabili -->
-    <div class="contenitore-offerte-filtri">
-        <div class="contenitore-filtri">
-            <h3>Filtri</h3>
+    <div class="rigacatalogo">
+        <div class="colonnacatalogo">
+            <a href="" style="text-decoration: none; color:black">
+                <div class="cardauto">
+                    <img class="imgauto_catalogo" src="{{asset('assets/img/ferrari_purosangue.jpg')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
+                    <div class="nomeauto">
+                        <p style="">Auto 1</p>
+                    </div>
+                </div>
+            </a>
         </div>
-        <div class="contenitore-offerte">
-            <div class="rigacatalogo">
-                <div class="colonnacatalogo">
-                    <a href="" style="text-decoration: none; color:black">
-                        <div class="cardauto">
-                            <img class="imgauto_catalogo" src="{{asset('assets/img/ferrari_purosangue.jpg')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
-                            <div class="nomeauto">
-                                <p style="">Auto 1</p>
-                            </div>
-                        </div>
-                    </a>
+        <div class="colonnacatalogo">
+            <a href="" style="text-decoration: none; color:black">
+                <div class="cardauto">
+                    <img class="imgauto_catalogo" src="{{asset('assets/img/fiat_punto.jpeg')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
+                    <div class="nomeauto">
+                        <p style="">Auto 2</p>
+                    </div>
                 </div>
-
-                <div class="colonnacatalogo">
-                    <a href="" style="text-decoration: none; color:black">
-                        <div class="cardauto">
-                            <img class="imgauto_catalogo" src="{{asset('assets/img/fiat_punto.jpeg')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
-                            <div class="nomeauto">
-                                <p style="">Auto 2</p>
-                            </div>
-                        </div>
-                    </a>
+            </a>
+        </div>
+        <div class="colonnacatalogo">
+            <a href="" style="text-decoration: none; color:black">
+                <div class="cardauto">
+                    <img class="imgauto_catalogo" src="{{asset('assets/img/ferrari_portofino.webp')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
+                    <div class="nomeauto">
+                        <p style="">Auto 3</p>
+                    </div>
                 </div>
-
-                <div class="colonnacatalogo">
-                    <a href="" style="text-decoration: none; color:black">
-                        <div class="cardauto">
-                            <img class="imgauto_catalogo" src="{{asset('assets/img/ferrari_portofino.webp')}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
-                            <div class="nomeauto">
-                                <p style="">Auto 3</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-            </div>
+            </a>
         </div>
     </div>
-
 @endsection
