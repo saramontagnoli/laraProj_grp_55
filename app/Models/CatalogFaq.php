217f5817dd50a9d3c81c68aa1c2fb025502a6e24
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 15:15:46 +0200

    Commenti aggiunti a CatalogFaq.php

--- app/Models/CatalogFaq.php
+++ app/Models/CatalogFaq.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use App\Models\Resources\Faq;
 
+/*
+ * Classe CatalogFaq con un singolo metodo getFaq
+ */
 class CatalogFaq {
 
+    /*
+     * Metodo che permette di restituire tutte le faq contenute all'interno della tabella faq del database
+     */
     public function getFaq() {
         return faq::all();
     }
