1895645c6b6b07239ced50b54f8c7067b67da1ae
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 17:03:36 +0200

    Commenti al controller del catalogo auto che contiene la vista completa del catalogo e la vista dell'auto singola

--- app/Http/Controllers/ControllerCatalogoAuto.php
+++ app/Http/Controllers/ControllerCatalogoAuto.php
@@ -5,25 +5,43 @@ namespace App\Http\Controllers;
 use App\Models\CatalogoAuto;
 use Illuminate\Support\Facades\Log;
 
-
+/*
+ * Classe Controller per il catalogo auto
+ */
 class ControllerCatalogoAuto extends Controller
 {
-    protected $_catalogModel;
+    //definizione della variabile che conterrà il catalogo completo delle auto
+    protected $_catalogAuto;
 
+    /*
+     * Costruttore della classe ControllerCatalogoAuto
+     */
     public function __construct() {
-        $this->_catalogModel = new CatalogoAuto();
+        //la variabile viene inizializzata come variabile di CatalogoAuto()
+        $this->_catalogAuto = new CatalogoAuto();
     }
 
+    /*
+     * Metodo che consente di mostrare tutte le auto mediante la vista catalogoauto
+     */
     public function showCatalogoAuto()
     {
-        $data = $this->_catalogModel->getCatalogoAuto();
+        //estrazione delle auto mediante il metodo getCatalogoAuto() della classe CatalogoAuto()
+        $data = $this->_catalogAuto->getCatalogoAuto();
 
+        //ritorno della vista del catalogo completo di tutte le auto noleggiabili
         return view('catalogoauto', compact('data'));
     }
 
+    /*
+     * Metodo che consente di mostrare una singola auto tra tutte quelle del catalogo
+     */
     public function showCatalogoAutoSpec($codice_auto)
     {
-        $data = $this->_catalogModel->getAutoSpec($codice_auto);
+        //estrazione dell'auto specifica selezionata con il metodo getAutoSpec() della classe CatalogoAuto()
+        $data = $this->_catalogAuto->getAutoSpec($codice_auto);
+
+        //ritorno della vista dell'auto singola contenente tutte le informazioni
         return view('autosingola', compact('data'));
     }
 
