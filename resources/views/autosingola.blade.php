5c6ffdf214c10f634203f8624622b910eff678b7
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 17:49:15 +0200

    Sistemazione commenti alla vista autosingola.blade.php

--- resources/views/autosingola.blade.php
+++ resources/views/autosingola.blade.php
@@ -19,21 +19,30 @@
     <!-- Se non c'è alcun errore si procede a stampare tutte le informazioni dell'auto -->
     @else
         @foreach($data as $auto)
+            <!-- Definizione della sezione della riga -->
             <div class="riga">
+
+                <!-- Definizione della colonna a sinistra -->
                 <div class="colonna colonna1">
+                    <!-- Targa dell'auto selezionata -->
                     <h1 style="text-align: center" class="titolo_info"><b>{{$auto->targa}}</b></h1>
                     <br>
+                    <!-- Immagine dell'auto selezionata -->
                     <img class="immagine_chisiamo" style="padding-left: 70px" src="{{ asset($auto->foto_auto) }}" alt="ImmagineAuto" height="200px">
                 </div>
 
+                <!-- Definizione della colonna a destra -->
                 <div class="colonna colonna2">
                     <br><br>
                     <br><br>
                     <br><br>
                     <br><br>
 
+                    <!-- Sezione della scheda tecnica dell'auto -->
                     <div style="font-weight: bolder; font-size: xx-large">SCHEDA AUTO</div>
                     <br><br><br>
+
+                    <!-- Descrizione dell'allestimento dell'auto -->
                     <div style="background-color:#EFEFEF">
                         <br>
                         <p style="font-weight: bold">Allestimento Auto:</p>
@@ -41,11 +50,15 @@
                         <br>
                     </div>
                     <hr>
+
+                    <!-- Costo dell'auto al giorno -->
                     <div style="background-color:#EFEFEF">
                         <p style="font-weight: bold">Costo/giorno:</p>
                         <p style="font-size: 14pt">{{$auto->costo_giorno}}</p>
                     </div>
                     <hr>
+
+                    <!-- Numero di posti dell'auto -->
                     <div style="background-color:#EFEFEF">
                         <p style="font-weight: bold">Numero di posti:</p>
                         <p style="font-size: 14pt">{{$auto->num_posti}}</p>
