3e113ae0ab80763c43d3c90fe33caf2f95bf0847
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 16:13:03 +0200

    Correzione ControllerCatalogoAuto.php

--- app/Http/Controllers/ControllerCatalogoAuto.php
+++ app/Http/Controllers/ControllerCatalogoAuto.php
@@ -11,7 +11,7 @@ class ControllerCatalogoAuto extends Controller
     protected $_catalogModel;
 
     public function __construct() {
-        $this->_catalogModel = new CatalogoAuto;
+        $this->_catalogModel = new CatalogoAuto();
     }
 
     public function showCatalogoAuto()
