cd2034004cc7c0ef20fa69687d5a18b94cf2585c
giadarem <giadaremedia@gmail.com>
2023-08-07 12:43:38 +0200

    come noleggiare

---
+++ resources/views/comenoleggiare.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.struttura')
+
+@section('content')
+
+    <!-- Titolo della pagina COME NOLEGGIARE con icone di profilo -->
+    <h2 class="posizione_cx titolo_info"><i class="fa fa-user"></i>&nbsp; COME NOLEGGIARE  &nbsp;<i class="fa fa-user"></i></h2>
+
+    <h3 style="text-align: center">Scegli l'auto dei tuoi sogni, noleggiarla sarà una passegiata!</h3>
+    <p style="text-align: center">Se non lo hai ancora fatto <a href="">registrati</a> gratuitamente al nostro sito, ci vorranno pochi secondi.
+        <br>Altrimenti, ti basterà accedere scegliere l'auto e il periodo in cui desideri noleggiarla; al resto ci penseremo noi!</p>
+    <!-- A capo per spaziature e formato della pagina -->
+    <br>
+@endsection
+
