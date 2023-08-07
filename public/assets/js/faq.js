623bed9e4e239c4dd8411d31aada81ad25ff72b9
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 14:54:27 +0200

    Fine aggiunta commenti al codice JS

--- public/assets/js/faq.js
+++ public/assets/js/faq.js
@@ -1,6 +1,6 @@
 /*
     Codice JavaScript associato alla pagina delle F.A.Q.
-    Permette di cliccare sulla domanda di interesse e di far apparire la rispettiva risposta
+    Permette di cliccare sulla domanda di interesse e di far apparire o sparire la rispettiva risposta
 */
 
 //dichiarazione variabile domande, vengono presi tutti gli elementi definiti con la classe "domande" che identifica una singola F.A.Q.
@@ -15,12 +15,17 @@ for (i = 0; i < domande.length; i++) {
     //ad ogni elemento estratto dalla collezione "domande" si associa un gestore di eventi CLICK
     //quando si fa click sopra quell'oggetto allora viene eseguita la funzione definita in seguito
     domande[i].addEventListener("click", function() {
-        //
-        this.classList.toggle("active");
+        //viene aggiunta o tolta la classe active all'elemento che viene cliccato
+        this.classList.toggle("active");7
+
+        //viene assegnato a risposta l'elemento HTML subito successivo (ovvero la risposta alla domande che si sta analizzando)
         var risposta = this.nextElementSibling;
+
+        //viene verificato se è già definita un'altezza massima inline, se sì viene impostata a null rimuovendola
         if (risposta.style.maxHeight) {
             risposta.style.maxHeight = null;
         } else {
+            //in caso contrario, se l'altezza massima non è definita inline allora viene impostata secondo lo scrollHeight
             risposta.style.maxHeight = risposta.scrollHeight + "px";
         }
     });
