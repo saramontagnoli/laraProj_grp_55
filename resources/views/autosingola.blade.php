dca5e927028e63b76c223123a847b17f1fbfb8b7
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 16:39:22 +0200

    Sistemazione commenti per vista autosingola.blade.php

--- resources/views/autosingola.blade.php
+++ resources/views/autosingola.blade.php
@@ -3,15 +3,20 @@
 
 <!-- Definizione della sezione del contenuto della pagina della singola auto -->
 @section('content')
-
+    <!-- Formattazione dello spazio verticale -->
     <br>
 
+    <!-- Button indietro che permette di tornare al catalogo generale delle auto -->
     <a href="{{url('/catalogoauto')}}" class="buttonindietro">&laquo; INDIETRO </a>
 
+    <!-- Se non sono stati trovati i dati dell'auto -->
     @if(count($data) < 1)
+        <!-- Viene stampato un messaggio di errore -->
         <div>
             <strong>Sorry!</strong> No Product Found.
         </div>
+
+    <!-- Se non c'è alcun errore si procede a stampare tutte le informazioni dell'auto -->
     @else
         @foreach($data as $auto)
             <p> Targa: {{$auto->targa}} </p>
