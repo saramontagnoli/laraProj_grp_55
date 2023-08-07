723a19463138005826503236c40808f95c0d7fa9
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 17:43:10 +0200

    Sistemazione scheda singola auto

--- resources/views/autosingola.blade.php
+++ resources/views/autosingola.blade.php
@@ -20,6 +20,37 @@
     @else
         @foreach($data as $auto)
             <div class="riga">
+                <div class="colonna colonna1">
+                    <h1 style="text-align: center" class="titolo_info"><b>{{$auto->targa}}</b></h1>
+                    <br>
+                    <img class="immagine_chisiamo" style="padding-left: 70px" src="{{ asset($auto->foto_auto) }}" alt="ImmagineAuto" height="200px">
+                </div>
+
+                <div class="colonna colonna2">
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+
+                    <div style="font-weight: bolder; font-size: xx-large">SCHEDA AUTO</div>
+                    <br><br><br>
+                    <div style="background-color: 	#EFEFEF">
+                        <br>
+                        <p style="font-weight: bold">Allestimento Auto:</p>
+                        <p style="font-size: 14pt">{{$auto->allestimento}}</p>
+                        <br>
+                    </div>
+                    <hr>
+                    <div style="background-color: 	#EFEFEF">
+                        <p style="font-weight: bold">Costo/giorno:</p>
+                        <p style="font-size: 14pt">{{$auto->costo_giorno}}</p>
+                    </div>
+                    <hr>
+                    <div style="background-color: 	#EFEFEF">
+                        <p style="font-weight: bold">Numero di posti:</p>
+                        <p style="font-size: 14pt">{{$auto->num_posti}}</p>
+                    </div>
+                </div>
 
             </div>
         @endforeach
