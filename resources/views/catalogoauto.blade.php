683b41779d8eb46cb3cfe59e8b59d80af3f2d579
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 16:52:41 +0200

    Sistemazione commenti per vista catalogoauto.blade.php

--- resources/views/catalogoauto.blade.php
+++ resources/views/catalogoauto.blade.php
@@ -5,12 +5,25 @@
 @section('content')
 
     <!-- Sezione dedicata al catalogo delle auto noleggiabili -->
+
+    <!-- Definizione della riga del catalogo -->
     <div class="rigacatalogo">
+
+        <!-- Per ogni auto estratta dalla tabella del database viene stampata la relativa card -->
         @foreach ($data as $auto)
+
+            <!--  -->
             <div class="colonnacatalogo">
+                <!-- Definizione del link cliccabile per ogni auto -->
                 <a href="{{ url('/catalogoauto/'.$auto->codice_auto) }}" style="text-decoration: none; color:black">
+
+                    <!-- Definizione della card dedll'auto con le informazioni -->
                     <div class="cardauto">
+
+                        <!-- Inserimento dell'immagine dal path definito all'interno del database -->
                         <img class="imgauto_catalogo" src="{{$auto->foto_auto}}" alt="IMMAGINE NON DISPONIBILE AL MOMENTO">
+
+                        <!-- Inserimento della targa dell'auto -->
                         <div class="nomeauto">
                             <p style="">{{$auto->targa}}</p>
                         </div>
