bb13608b167061cef4c40d753a7c50d4d10af352
saramontagnoli <s1098041@studenti.univpm.it>
2023-08-07 17:04:33 +0200

    Correzione commenti nel model Auto.php

--- app/Models/Resources/Auto.php
+++ app/Models/Resources/Auto.php
@@ -5,14 +5,14 @@ namespace App\Models\Resources;
 use Illuminate\Database\Eloquent\Model;
 
 /*
- * Model per la definizione delle F.A.Q.
+ * Model per la definizione delle AUTO
  */
 class Auto extends Model {
 
-    //associazione alla tabella faq
+    //associazione alla tabella auto del database
     protected $table = 'auto';
 
-    //definizione della primary key = codice_faq
+    //definizione della primary key = codice_auto
     protected $primaryKey = 'codice_auto';
     public $timestamps = false;
 }
