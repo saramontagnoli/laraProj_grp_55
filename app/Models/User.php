2bf50c81d353f7d3737e11eb9e3319990b35cce0
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-03 17:50:04 +0200

    Correzione Model di User

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'surname',
+        'id',
+        'nome',
+        'cognome',
         'email',
         'username',
         'password',
