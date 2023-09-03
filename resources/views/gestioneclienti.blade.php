0e5039d2e9fc5b337e2d57a2e4f083c34e100b86
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-03 14:08:59 +0200

    Vista della gestione dei clienti funzionante, commenti completi

--- resources/views/gestioneclienti.blade.php
+++ resources/views/gestioneclienti.blade.php
@@ -1,31 +1,42 @@
 @extends('layouts.struttura')
+
+<!-- Definizione della sezione del contenuto della pagina della gestione dei clienti -->
 @section('content')
+
+    <!-- Titolo della pagina dedicata alla gestione dei clienti da parte dell'admin -->
     <h1 class="titolo_info">GESTIONE CLIENTI SITO</h1>
+
+    <!-- Definizione della tabella contenente tutti i clienti registrati al sito -->
     <table class="tabella_noleggi">
-    <thead>
-    <tr>
-        <th>Username</th>
-        <th>Nome</th>
-        <th>Cognome</th>
-        <th>Data_nascita</th>
-        <th>Elimina</th>
-    </tr>
-    </thead>
-    <tbody>
-    <!-- Auto presenti nella tabella autoS -->
-    @foreach($clienti as $clie)
-        <tr>
-            <td>{{$clie['username']}}</td>
-            <td>{{$clie['nome']}}</td>
-            <td>{{$clie['cognome']}}</td>
-            <td>{{$clie['data_nascita']}}</td>
-            <td><a href="{{ route('eliminaCliente', ['id' => $clie['id']]) }}" onclick="return deleteCliente()" class="tabella_link">Elimina</a></td>
-        </tr>
-    @endforeach
-    </tbody>
+
+        <!-- Definizione dell'intestazione della tabella con le informazioni dell'utente -->
+        <thead>
+            <tr>
+                <th>Username</th> <!-- Username -->
+                <th>Nome</th> <!-- Nome -->
+                <th>Cognome</th> <!-- Cognome -->
+                <th>Data_nascita</th> <!-- Data di nascita -->
+                <th>Elimina</th> <!-- Link di eliminazione dell'utente -->
+            </tr>
+        </thead>
+
+        <!-- Definizione del contenuto della tabella, righe contenenti tutti i clienti del sito -->
+        <tbody>
+        <!-- Scorrimento dell'array contenente tutti i cienti estratti -->
+        @foreach($clienti as $clie)
+            <tr>
+                <td>{{$clie['username']}}</td>
+                <td>{{$clie['nome']}}</td>
+                <td>{{$clie['cognome']}}</td>
+                <td>{{$clie['data_nascita']}}</td>
+                <td><a href="{{ route('eliminaCliente', ['id' => $clie['id']]) }}" onclick="return deleteCliente()" class="tabella_link">Elimina</a></td>
+            </tr>
+        @endforeach
+        </tbody>
     </table>
     <br><br>
-    <!-- è da sistemare questo messaggio d errore -->
+
+    <!-- Messaggio di errore -->
     @if(session('message'))
         <div class="alert alert-success">
             {{ session('message') }}
