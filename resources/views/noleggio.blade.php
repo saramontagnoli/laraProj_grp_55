38f9c8b59118ecc1efa369ab784d4e3aca4021f4
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-03 13:43:08 +0200

    Vista dell'avvenuto noleggio funzionante, commenti completi

--- resources/views/noleggio.blade.php
+++ resources/views/noleggio.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.struttura')
+
+<!-- Definizione della sezione del contenuto della pagina della conferma di avvenuto noleggio -->
 @section('content')
+
+    <!-- Stampa dello username dell'utente loggato per conferma di avvenuto noleggio -->
     <h2 class="titolo_info">{{ Auth::user()->username }} HAI NOLEGGIATO CORRETTAMENTE!</h2>
 
+    <!-- Sezione della pagina che riepiloga i dettagli del noleggio -->
     <div class="posizione_cx">
+
+        <!-- Titolo di riepilogo delle info del noleggio -->
         <p class="titolo_info">Riepilogo auto noleggiata: </p>
+
+        <!-- Stamap delle informazioni del noleggio avvenuto -->
         @foreach($cardAuto as $data)
             <div>
-                <p><b>Marca auto:</b>{{$data->nome_marca}}</p>
-                <p><b>Modello auto:</b> {{$data->nome_modello}}</p>
-                <p><b>Targa auto:</b> {{$data->targa}}</p>
-                <p><b>Periodo di noleggio: da</b> {{$data->data_inizio}} <b>a</b> {{$data->data_fine}}</p>
+                <p><b>Marca auto:</b>{{$data->nome_marca}}</p> <!--Nome della marca dell'auto noleggiata -->
+                <p><b>Modello auto:</b> {{$data->nome_modello}}</p> <!-- Nome del modello dell'auto noleggiata -->
+                <p><b>Targa auto:</b> {{$data->targa}}</p> <!-- Targa dell'auto noleggiata -->
+                <p><b>Periodo di noleggio: da</b> {{$data->data_inizio}} <b>a</b> {{$data->data_fine}}</p> <!-- Durata del noleggio dell'auto -->
                 <p></p>
             </div>
-
         @endforeach
     </div>
 
