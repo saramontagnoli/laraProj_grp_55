2c50b4e4b8d4f37ebfb1b677db1ecbe792d7f41a
giadarem <giadaremedia@gmail.com>
2023-09-04 19:51:19 +0200

    cose per il db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -65,74 +65,10 @@ class DatabaseSeeder extends Seeder
         /*
          * Inserimento dei dati predefiniti all'interno della tabella stato del database mediante insert
          */
-        DB::table('stato')->insert([
-            ['codice_stato' => 1, 'nome_stato' => 'Albania'],
-            ['codice_stato' => 2, 'nome_stato' => 'Andorra'],
-            ['codice_stato' => 3, 'nome_stato' => 'Armenia'],
-            ['codice_stato' => 4, 'nome_stato' => 'Austria'],
-            ['codice_stato' => 5, 'nome_stato' => 'Azerbaijan'],
-            ['codice_stato' => 6, 'nome_stato' => 'Bosnia-Erzegovina'],
-            ['codice_stato' => 7, 'nome_stato' => 'Belgio'],
-            ['codice_stato' => 8, 'nome_stato' => 'Bulgaria'],
-            ['codice_stato' => 9, 'nome_stato' => 'Croazia'],
-            ['codice_stato' => 10, 'nome_stato' => 'Cipro'],
-            ['codice_stato' => 11, 'nome_stato' => 'Danimarca'],
-            ['codice_stato' => 12, 'nome_stato' => 'Estonia'],
-            ['codice_stato' => 13, 'nome_stato' => 'Finlandia'],
-            ['codice_stato' => 14, 'nome_stato' => 'Francia'],
-            ['codice_stato' => 15, 'nome_stato' => 'Georgia'],
-            ['codice_stato' => 16, 'nome_stato' => 'Germania'],
-            ['codice_stato' => 17, 'nome_stato' => 'Grecia'],
-            ['codice_stato' => 18, 'nome_stato' => 'Islanda'],
-            ['codice_stato' => 19, 'nome_stato' => 'Irlanda'],
-            ['codice_stato' => 20, 'nome_stato' => 'Italia'],
-            ['codice_stato' => 21, 'nome_stato' => 'Lettonia'],
-            ['codice_stato' => 22, 'nome_stato' => 'Liechtestein'],
-            ['codice_stato' => 23, 'nome_stato' => 'Lituania'],
-            ['codice_stato' => 24, 'nome_stato' => 'Lussemburgo'],
-            ['codice_stato' => 25, 'nome_stato' => 'Malta'],
-            ['codice_stato' => 26, 'nome_stato' => 'Moldova'],
-            ['codice_stato' => 27, 'nome_stato' => 'Monaco'],
-            ['codice_stato' => 28, 'nome_stato' => 'Montenegro'],
-            ['codice_stato' => 29, 'nome_stato' => 'Norvegia'],
-            ['codice_stato' => 30, 'nome_stato' => 'Paesi Bassi'],
-            ['codice_stato' => 31, 'nome_stato' => 'Polonia'],
-            ['codice_stato' => 32, 'nome_stato' => 'Portogallo'],
-            ['codice_stato' => 33, 'nome_stato' => 'Regno Unito'],
-            ['codice_stato' => 34, 'nome_stato' => 'Romania'],
-            ['codice_stato' => 35, 'nome_stato' => 'Repubblica Ceca'],
-            ['codice_stato' => 36, 'nome_stato' => 'Russia'],
-            ['codice_stato' => 37, 'nome_stato' => 'San Marino'],
-            ['codice_stato' => 38, 'nome_stato' => 'Serbia'],
-            ['codice_stato' => 39, 'nome_stato' => 'Slovacchia'],
-            ['codice_stato' => 40, 'nome_stato' => 'Slovenia'],
-            ['codice_stato' => 41, 'nome_stato' => 'Spagna'],
-            ['codice_stato' => 42, 'nome_stato' => 'Svezia'],
-            ['codice_stato' => 43, 'nome_stato' => 'Svizzera'],
-            ['codice_stato' => 44, 'nome_stato' => 'Turchia'],
-            ['codice_stato' => 45, 'nome_stato' => 'Ucraina'],
-            ['codice_stato' => 46, 'nome_stato' => 'Ungheria'],
-            ['codice_stato' => 47, 'nome_stato' => 'Macedonia'],
-            ['codice_stato' => 48, 'nome_stato' => 'Paese non europeo']
-        ]);
-
 
         /*
          * Inserimento dei dati predefiniti all'interno della tabella comune del database mediante insert
          */
-        DB::table('comune')->insert([
-            ['codice_comune' => 1, 'CAP' => '60121', 'nome_comune' => 'Ancona', 'stato_ref' => 20],
-            ['codice_comune' => 2, 'CAP' => '63900', 'nome_comune' => 'Fermo', 'stato_ref' => 20],
-            ['codice_comune' => 3, 'CAP' => '63822', 'nome_comune' => 'Porto San Giorgio', 'stato_ref' => 20],
-            ['codice_comune' => 4, 'CAP' => '00042', 'nome_comune' => 'Roma', 'stato_ref' => 20],
-            ['codice_comune' => 5, 'CAP' => 'E1W', 'nome_comune' => 'Londra', 'stato_ref' => 33],
-            ['codice_comune' => 6, 'CAP' => '28001', 'nome_comune' => 'Madrid', 'stato_ref' => 41],
-            ['codice_comune' => 7, 'CAP' => 'VLT', 'nome_comune' => 'Valletta', 'stato_ref' => 25],
-            ['codice_comune' => 8, 'CAP' => '10115', 'nome_comune' => 'Berlino', 'stato_ref' => 16],
-            ['codice_comune' => 9, 'CAP' => '1001', 'nome_comune' => 'Tirana', 'stato_ref' => 1],
-            ['codice_comune' => 10, 'CAP' => '100', 'nome_comune' => 'Tokyo', 'stato_ref' => 48]
-        ]);
-
 
         /*
          * Inserimento dei dati predefiniti all'interno della tabella auto del database mediante insert
@@ -153,11 +89,11 @@ class DatabaseSeeder extends Seeder
          * Inserimento dei dati predefiniti all'interno della tabella users del database mediante insert
          */
         DB::table('users')->insert([
-            ['id' => 1, 'nome' => 'Sara', 'cognome' => 'Montagnoli', 'data_nascita' => '2001-10-31', 'username' => 'clieclie', 'password' => bcrypt('3Esp6L54'), 'role' => 'user', 'email' => 'sara@gmail.com', 'indirizzo' => 'via caso 13', 'occupazione_ref' => 4, 'comune_ref' => 2],
-            ['id' => 2, 'nome' => 'Giada', 'cognome' => 'Remedia', 'data_nascita' => '2001-09-26', 'username' => 'staffstaff', 'password' => bcrypt('3Esp6L54'), 'role' => 'staff', 'email' => 'giada@gmail.com', 'indirizzo' => 'via nulla 10', 'occupazione_ref' => 4, 'comune_ref' => 4],
-            ['id' => 3, 'nome' => 'Admin', 'cognome' => 'Admin', 'data_nascita' => '1990-11-11', 'username' => 'adminadmin', 'password' => bcrypt('3Esp6L54'), 'role' => 'admin', 'email' => 'admin@gmail.com', 'indirizzo' => 'via roma 11', 'occupazione_ref' => 1, 'comune_ref' => 1],
-            ['id' => 4, 'nome' => 'Luca', 'cognome' => 'Bianchi', 'data_nascita' => '1997-06-11', 'username' => 'lucabianchi', 'password' => bcrypt('ciao01!'), 'role' => 'user', 'email' => 'lucabianchi@gmail.com', 'indirizzo' => 'via napoli 11', 'occupazione_ref' => 2, 'comune_ref' => 3],
-            ['id' => 5, 'nome' => 'Marco', 'cognome' => 'Verdi', 'data_nascita' => '1998-01-27', 'username' => 'marcoverdi98', 'password' => bcrypt('ciaociao01!'), 'role' => 'user', 'email' => 'marcoverdi@gmail.com', 'indirizzo' => 'via torino 5', 'occupazione_ref' => 3, 'comune_ref' => 4]
+            ['id' => 1, 'nome' => 'Sara', 'cognome' => 'Montagnoli', 'data_nascita' => '2001-10-31', 'username' => 'clieclie', 'password' => bcrypt('3Esp6L54'), 'role' => 'user', 'email' => 'sara@gmail.com', 'indirizzo' => 'via caso 13', 'occupazione_ref' => 4, 'comune_ref' => 1001],
+            ['id' => 2, 'nome' => 'Giada', 'cognome' => 'Remedia', 'data_nascita' => '2001-09-26', 'username' => 'staffstaff', 'password' => bcrypt('3Esp6L54'), 'role' => 'staff', 'email' => 'giada@gmail.com', 'indirizzo' => 'via nulla 10', 'occupazione_ref' => 4, 'comune_ref' => 1002],
+            ['id' => 3, 'nome' => 'Admin', 'cognome' => 'Admin', 'data_nascita' => '1990-11-11', 'username' => 'adminadmin', 'password' => bcrypt('3Esp6L54'), 'role' => 'admin', 'email' => 'admin@gmail.com', 'indirizzo' => 'via roma 11', 'occupazione_ref' => 1, 'comune_ref' => 1003],
+            ['id' => 4, 'nome' => 'Luca', 'cognome' => 'Bianchi', 'data_nascita' => '1997-06-11', 'username' => 'lucabianchi', 'password' => bcrypt('ciao01!'), 'role' => 'user', 'email' => 'lucabianchi@gmail.com', 'indirizzo' => 'via napoli 11', 'occupazione_ref' => 2, 'comune_ref' => 1004],
+            ['id' => 5, 'nome' => 'Marco', 'cognome' => 'Verdi', 'data_nascita' => '1998-01-27', 'username' => 'marcoverdi98', 'password' => bcrypt('ciaociao01!'), 'role' => 'user', 'email' => 'marcoverdi@gmail.com', 'indirizzo' => 'via torino 5', 'occupazione_ref' => 3, 'comune_ref' => 1005]
         ]);
 
 
