3d39d132301419d917dfdfd7d7dc07fb23f3a269
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-05 16:08:35 +0200

    Sistemazione gestioneauto .blade.php

--- resources/views/gestioneauto.blade.php
+++ resources/views/gestioneauto.blade.php
@@ -1,40 +1,51 @@
+<!-- Si estende la struttura definita per le pagine del sito web -->
 @extends('layouts.struttura')
+
+<!-- Definizione della sezione del contenuto della pagina del login -->
 @section('content')
     <br>
+    <!-- Button per l'aggiunta di una nuova auto, redirezione alla rotta definita -->
     <a href="{{ route('aggiuntaAuto') }}">Aggiungi Auto</a>
 
     <br>
+
+    <!-- Definizione della tabella di gestione delle auto -->
     <table class="tabella_noleggi">
-    <thead>
-    <tr>
-        <th>Targa</th>
-        <th>Marca</th>
-        <th>Modello</th>
-        <th>Numero Posti</th>
-        <th>Allestimento</th>
-        <th>Costo Giornaliero</th>
-        <th>Modifica</th>
-        <th>Elimina</th>
-    </tr>
-    </thead>
-    <tbody>
-    <!-- Auto presenti nella tabella autoS -->
-    @foreach($listaAuto as $auto)
-        <tr>
-            <td>{{$auto['targa']}}</td>
-            <td>{{$auto['nome_marca']}}</td>
-            <td>{{$auto['nome_modello']}}</td>
-            <td>{{$auto['num_posti']}}</td>
-            <td>{{$auto['allestimento']}}</td>
-            <td>{{$auto['costo_giorno']}}€</td>
-            <td><a href="{{ route('getdatiauto', ['codice_auto' => $auto['codice_auto']]) }}">Modifica</a></td>
-            <td><a href="{{ route('eliminaauto', ['codice_auto' => $auto['codice_auto']]) }}" onclick="return confirmDelete()">Elimina</a></td>
-        </tr>
-    @endforeach
-    </tbody>
+
+        <!-- Definizione dell'intestazione della tabella -->
+        <thead>
+            <tr>
+                <th>Targa</th>
+                <th>Marca</th>
+                <th>Modello</th>
+                <th>Numero Posti</th>
+                <th>Allestimento</th>
+                <th>Costo Giornaliero</th>
+                <th>Modifica</th>
+                <th>Elimina</th>
+            </tr>
+        </thead>
+
+        <!-- Definizione del corpo della tabella contenente tutte le auto del catalogo -->
+        <tbody>
+        <!-- Auto presenti nella tabella autoS -->
+        @foreach($listaAuto as $auto)
+            <tr>
+                <td>{{$auto['targa']}}</td> <!-- Targa dell'auto -->
+                <td>{{$auto['nome_marca']}}</td> <!-- Nome della marca dell'auto -->
+                <td>{{$auto['nome_modello']}}</td> <!-- Nome del modello dell'auto -->
+                <td>{{$auto['num_posti']}}</td> <!-- Numero posti dell'auto -->
+                <td>{{$auto['allestimento']}}</td> <!-- Descrizione dell'allestimento dell'auto -->
+                <td>{{$auto['costo_giorno']}}€</td> <!-- Costo al giorno dell'auto -->
+                <td><a href="{{ route('getdatiauto', ['codice_auto' => $auto['codice_auto']]) }}">Modifica</a></td> <!-- Link di redirezione alla rotta di modifica dell'auto selezionata -->
+                <td><a href="{{ route('eliminaauto', ['codice_auto' => $auto['codice_auto']]) }}" onclick="return confirmDelete()">Elimina</a></td> <!-- Link di eliminazione dell'auto selezionata -->
+            </tr>
+        @endforeach
+        </tbody>
     </table>
     <br><br>
-    <!-- è da sistemare questo messaggio d errore -->
+
+    <!-- Stampa di eventuali messaggi che vengono ritornati a questa vista -->
     @if(session('message'))
         <div class="alert alert-success">
             {{ session('message') }}
