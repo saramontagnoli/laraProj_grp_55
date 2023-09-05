9bfb0cddaa78a7305867443b27dea8a5c9784cf0
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-05 11:16:17 +0200

    Sistemazione struttura della pagina HTML

--- resources/views/layouts/struttura.blade.php
+++ resources/views/layouts/struttura.blade.php
@@ -18,8 +18,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Inclusione di tutti i file JS esterni -->
-
-
     <script src="{{ asset('assets/js/app.js') }}"></script>
 
 </head>
