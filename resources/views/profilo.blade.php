434c034a10e83bab4cf1a01aa5b641606b9c5d31
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-05 16:34:40 +0200

    Sistemazione profilo .blade.php

--- resources/views/profilo.blade.php
+++ resources/views/profilo.blade.php
@@ -6,11 +6,14 @@
 
     <!-- Riepilogo del profilo dell'utente loggato -->
     <div class="posizione_cx">
-        <img alt="immagine" src="{{asset('assets/img/icona_chisiamo.png')}}">
-        <h2 style="text-align: center; font-size: x-large">Nome: {{ Auth::user()->nome }}</h2>
-        <h2 style="text-align: center; font-size: x-large">Cognome: {{ Auth::user()->cognome }}</h2>
-        <h2 style="text-align: center; font-size: x-large">Data nascita: {{ Auth::user()->data_nascita }}</h2>
-        <h2 style="text-align: center; font-size: x-large">E-mail: {{ Auth::user()->email }}</h2>
+        <img alt="immagine" src="{{asset('assets/img/icona_chisiamo.png')}}"> <!-- Icona del profilo -->
+        <h2 style="text-align: center; font-size: x-large">E-mail: {{ Auth::user()->username }}</h2> <!-- Username dell'utente -->
+        <h2 style="text-align: center; font-size: x-large">Nome: {{ Auth::user()->nome }}</h2> <!-- Nome dell'utente -->
+        <h2 style="text-align: center; font-size: x-large">Cognome: {{ Auth::user()->cognome }}</h2> <!-- Cognome dell'utente -->
+        <h2 style="text-align: center; font-size: x-large">Data nascita: {{ Auth::user()->data_nascita }}</h2> <!-- Data nascita dell'utente -->
+        <h2 style="text-align: center; font-size: x-large">E-mail: {{ Auth::user()->email }}</h2> <!-- Email dell'utente -->
+        <h2 style="text-align: center; font-size: x-large">E-mail: {{ Auth::user()->indirizzo }}</h2> <!-- Indirizzo dell'utente -->
+
 
         <div class="posizione_cx">
             <!-- Button di modifica dei dati personali dell'utente autenticato -->
