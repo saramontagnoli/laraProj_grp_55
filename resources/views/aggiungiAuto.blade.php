754a2fc4455ea783da081ac1d04d8d3f223f671f
saramontagnoli <s1098041@studenti.univpm.it>
2023-09-05 16:15:43 +0200

    Sistemazione aggiungiAuto .blade.php

--- resources/views/aggiungiAuto.blade.php
+++ resources/views/aggiungiAuto.blade.php
@@ -7,23 +7,28 @@
 @section('content')
 
     <div style="text-align: center">
-        <!-- Intestazione della form di login -->
-        <h1 class="titolo_info">Aggiungi Auto</h1>
+        <!-- Titolo per l'aggiunta di una nuova auto -->
+        <h1 class="titolo_info">AGGIUNTA NUOVA AUTO</h1>
+
+        <!-- Definizione della sezione di aggiunta di una nuova auto -->
         <p>Utilizza questa form per aggiungere un'auto:</p>
         <br>
+
         <div style="text-align: left">
-            <!-- Button indietro che permette di tornare alla pagina di gestione delle F.A.Q. -->
+            <!-- Button indietro che permette di tornare alla pagina di gestione delle auto -->
             <a href="{{ route('gestioneauto') }}" class="bottone">&laquo; INDIETRO </a>
         </div>
+
+        <!-- Sezione dedicata alla form di aggiuta di una nuova auto -->
         <div>
             <div>
-                <!-- Apertura della form per il login con redirezione alla rotta di login -->
+                <!-- Apertura della form per l'aggiunta di una nuova auto con redirezione alla rotta di aggiuntaAuto -->
                 {{ Form::open(array('url' => '/aggiuntaAuto', 'enctype' => 'multipart/form-data', 'method' => 'PUT')) }}
                 <div>
-                    <!-- Definizione della label per l'inserimento dello username -->
+                    <!-- Definizione della label per l'inserimento della targa -->
                     {{ Form::label('targa', 'Targa') }}
                     <br>
-                    <!-- Campo di inserimento dello username, avente come id "username" -->
+                    <!-- Campo di inserimento della targa, avente come id "targa" -->
                     {{ Form::text('targa', '', ['id' => 'targa', 'class' => 'campo_form']) }}
 
                     <!-- Se vengono rilevati degli errori allora vengono stampati -->
@@ -39,7 +44,7 @@
                 <br>
 
                 <div>
-                    <!-- Definizione della label per la scelta del modello -->
+                    <!-- Definizione della label per la scelta del modello dell'auto -->
                     {{ Form::label('modello_ref', 'Modello') }}
                     <br>
                     <!-- Campo di selezione del modello, avente come id "modello_ref" -->
@@ -48,7 +53,6 @@
                         @foreach ($modelli as $modello)
                             <option value="{{ $modello->codice_modello }}">{{ $modello->nome_modello }}</option>
                         @endforeach
-
                     </select>
 
                     <!-- Se vengono rilevati degli errori, vengono stampati -->
@@ -60,12 +64,14 @@
                         </ul>
                     @endif
                 </div>
+
                 <br>
+
                 <div>
-                    <!-- Definizione della label per l'inserimento della password -->
+                    <!-- Definizione della label per l'inserimento del numero di posti -->
                     {{ Form::label('num_posti', 'Numero di Posti') }}
                     <br>
-                    <!-- Campo di inserimento della password, avente come id "password" -->
+                    <!-- Campo di inserimento del numero di posti, avente come id "num_posti" -->
                     {{ Form::number('num_posti', '',['id' => 'num_posti' , 'class' => 'campo_form']) }}
 
                     <!-- Se vengono rilevati degli errori allora vengono stampati -->
@@ -81,10 +87,10 @@
                 <br>
 
                 <div>
-                    <!-- Definizione della label per l'inserimento della password -->
+                    <!-- Definizione della label per l'inserimento del costo/giorno dell'auto -->
                     {{ Form::label('costo_giorno', 'Costo Giornaliero') }}
                     <br>
-                    <!-- Campo di inserimento della password, avente come id "password" -->
+                    <!-- Campo di inserimento del costo giornaliero, avente come id "costo_giorno" -->
                     {{ Form::number('costo_giorno','', ['id' => 'costo_giorno' , 'class' => 'campo_form']) }}
 
                     <!-- Se vengono rilevati degli errori allora vengono stampati -->
@@ -100,10 +106,10 @@
                 <br>
 
                 <div>
-                    <!-- Definizione della label per l'inserimento della password -->
+                    <!-- Definizione della label per l'inserimento della descrizione dell'allestimento dell'auto -->
                     {{ Form::label('allestimento', 'Allestimento') }}
                     <br>
-                    <!-- Campo di inserimento della password, avente come id "password" -->
+                    <!-- Campo di inserimento dell'allestimento, avente come id "allestimento" -->
                     {{ Form::text('allestimento','', ['id' => 'allestimento' , 'class' => 'campo_form']) }}
 
                     <!-- Se vengono rilevati degli errori allora vengono stampati -->
@@ -119,8 +125,10 @@
                 <br>
 
                 <div>
-                    <!-- Definizione della label per l'inserimento della password -->
+                    <!-- Definizione della label per l'inserimento dell'immagine dell'auto -->
                     {{ Form::label('foto_auto','Immagine auto', ['class' => 'label-input']) }}
+
+                    <!-- Campo di inserimento dell'immagine dell'auto, avente come id "foto_auto" -->
                     {{ Form::file('foto_auto', array('required' => 'required')) }}
 
                     <!-- Se vengono rilevati degli errori allora vengono stampati -->
@@ -136,11 +144,11 @@
                 <br>
 
                 <div>
-                    <!-- Bottone di submit per l'invio dei dati inseriti nella form -->
+                    <!-- Bottone di submit per l'invio dei dati inseriti nella form di aggiunta -->
                     {{ Form::submit('Aggiungi Auto', ['class' => 'bottone']) }}
                 </div>
 
-
+                <!-- Tag di chiusura della Form di aggiunta dell'auto -->
                 {{ Form::close() }}
             </div>
         </div>
