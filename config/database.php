506c325ae4b054d0af0927235f53dc819060858a
saramontagnoli <s1098041@studenti.univpm.it>
2023-10-11 14:49:00 +0200

    Commit restore

--- config/database.php
+++ config/database.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-require( __DIR__ . '/../../include/connect.php');
 
 return [
 
@@ -47,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $HOST,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $DB,
-            'username' => $USER,
-            'password' => $PASSWORD,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
